45c80d87b73dcc9d4a21826fbc001aaa594f2804
Sean Lavine <freewil@users.noreply.github.com>
2019-06-27 22:58:42 -0400

    start: let

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 
 module.exports = function blocked (fn, options) {
     const opts = options || {};
-    const start = process.hrtime();
     const interval = opts.interval || 2500;
     const threshold = opts.threshold || 200;
 
+    let start = process.hrtime();
     return setInterval(function blockedTimer () {
         const delta = process.hrtime(start);
         const nanosec = delta[0] * 1e9 + delta[1];
